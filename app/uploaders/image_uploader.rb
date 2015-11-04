835e2ddc9d907fcbd1ee9be8ce3678cebe768f51
maangie <maangie906@gmail.com>
2015-11-04 21:03:36 +0900

    modify spec for cloudinary.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,7 +20,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   # ActionController::Base.helpers.asset_path(
+  #   #   "fallback/" + [version_name, "default.png"].compact.join('_')
+  #   # )
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
@@ -44,9 +46,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # Avoid using model.id or version_name here, see uploader/store.rb
+  # for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
 
+  if Rails.env.test?
+    def public_id
+      sanitized_file.original_filename
+    end
+  end
 end
